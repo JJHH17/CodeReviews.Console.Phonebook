188e50b4609eaa5ee3da2a596e7c19b92e87c776
James Hatfield <james.hatfield17@icloud.com>
2025-09-11 08:02:57 +0100

    Created base class for phonebook

--- Phonebook.JJHH17/Phonebook.JJHH17/Program.cs
+++ Phonebook.JJHH17/Phonebook.JJHH17/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+// Entity Framework installed
+// SQL Server used
+// app.config file used for SQL credentials
+
+namespace Phonebook.JJHH17
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Howdy, all!");
+        }
+    }
+}
