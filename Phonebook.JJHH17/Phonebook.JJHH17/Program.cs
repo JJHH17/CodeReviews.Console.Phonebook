e5da972942bbc150c4a4909bbb151f2421a40c72
James Hatfield <james.hatfield17@icloud.com>
2025-09-11 08:25:58 +0100

    Added database connection string from app.config file

--- Phonebook.JJHH17/Phonebook.JJHH17/Program.cs
+++ Phonebook.JJHH17/Phonebook.JJHH17/Program.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Protocols;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 
-// Entity Framework installed
+// Entity Framework installed, both framework and SQL Server EF Packages
 // SQL Server used
 // app.config file used for SQL credentials
 
@@ -13,6 +15,28 @@ namespace Phonebook.JJHH17
         public static void Main(string[] args)
         {
             Console.WriteLine("Howdy, all!");
+        }
+    }
+
+    public class PhoneBook
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+
+    public class PhoneBookContext : DbContext
+    {
+        private static readonly string server = ConfigurationManager.AppSettings["Server"];
+        private static readonly string databaseInstance = ConfigurationManager.AppSettings["DatabaseName"];
+        public static string connectionString = $@"Server=({server})\{databaseInstance};Integrated Security=true;";
+
+        public DbSet<PhoneBook> PhoneBooks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Connection string to be added");
         }
     }
 }
