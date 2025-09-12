9a2dbe55dac5804450170133cf0ba0dc8c2fbb76
James Hatfield <james.hatfield17@icloud.com>
2025-09-12 11:14:24 +0100

    Removed phonebook and db class into new file

--- Phonebook.JJHH17/Phonebook.JJHH17/Program.cs
+++ Phonebook.JJHH17/Phonebook.JJHH17/Program.cs
@@ -22,27 +22,4 @@ namespace Phonebook.JJHH17
             UserInterface.Menu();
         }
     }
-
-    public class PhoneBook
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
-
-    }
-
-    public class PhoneBookContext : DbContext
-    {
-        private static readonly string server = ConfigurationManager.AppSettings["Server"];
-        private static readonly string databaseInstance = ConfigurationManager.AppSettings["DatabaseName"];
-        public static string connectionString = $@"Server=({server})\{databaseInstance};Integrated Security=true;";
-
-        public DbSet<PhoneBook> PhoneBooks { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer(connectionString);
-        }
-    }
 }
