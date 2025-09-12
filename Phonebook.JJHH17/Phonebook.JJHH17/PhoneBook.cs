dfd3891115440dbb54c9220e3110bc421d9dcead
James Hatfield <james.hatfield17@icloud.com>
2025-09-12 11:14:13 +0100

    Separated phonebook class into new file

---
+++ Phonebook.JJHH17/Phonebook.JJHH17/PhoneBook.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace Phonebook.JJHH17
+{
+    public class PhoneBook
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+    }
+
+    public class PhoneBookContext : DbContext
+    {
+        private static readonly string server = ConfigurationManager.AppSettings["Server"];
+        private static readonly string databaseInstance = ConfigurationManager.AppSettings["DatabaseName"];
+        public static string connectionString = $@"Server=({server})\{databaseInstance};Integrated Security=true;";
+
+        public DbSet<PhoneBook> PhoneBooks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
+
